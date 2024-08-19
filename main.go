55d9431af0f40aa7395eca758f8bcc41d42b113d
Abhiram <abhiram090601@gmail.com>
2024-08-19 19:47:42 +0530

    add debug mode which deletes database.json

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+)
+
 func main() {
+	dbg := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+	if *dbg {
+		log.Println("USING DEBUG MODE")
+		log.Println("Deleting database.json...")
+		err := os.Remove(databasePath)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
 	startServer()
 }
